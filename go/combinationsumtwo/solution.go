44ad3d3ec4690b8cc2414f11826c603017a0048d
castlele <castlelecs@gmail.com>
2025-06-16 09:28:36 +0300

    [feat]: add combination sum 2 problem

--- go/combinationsumtwo/solution.go
+++ go/combinationsumtwo/solution.go
@@ -1,7 +1,35 @@
 package combinationsumtwo
 
+import "sort"
+
 func combinationSum2(candidates []int, target int) [][]int {
 	var res [][]int
+	var path []int
+	var backtrace func(int, int)
+	sort.Ints(candidates)
+
+	backtrace = func(start int, remaining int) {
+		if remaining == 0 {
+			res = append(res, append([]int{}, path...))
+			return
+		}
+
+		if remaining < 0 {
+			return
+		}
+
+		for i := start; i < len(candidates); i++ {
+			if i > start && candidates[i] == candidates[i-1] {
+				continue
+			}
+
+			path = append(path, candidates[i])
+			backtrace(i+1, remaining-candidates[i])
+			path = path[:len(path)-1]
+		}
+	}
+
+	backtrace(0, target)
 
 	return res
 }
--- go/combinationsumtwo/solution_test.go
+++ go/combinationsumtwo/solution_test.go
@@ -1,13 +1,28 @@
 package combinationsumtwo
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func TestCombinationTwo(t *testing.T) {
 	cases := []struct {
 		candidates []int
-		target int
-		output [][]int
-	} {
-		{},
+		target     int
+		output     [][]int
+	}{
+		{[]int{10, 1, 2, 7, 6, 1, 5}, 8, [][]int{{1, 1, 6}, {1, 2, 5}, {1, 7}, {2, 6}}},
+		{[]int{2, 5, 2, 1, 2}, 5, [][]int{{1, 2, 2}, {5}}},
+	}
+
+	for i, ts := range cases {
+		t.Run(fmt.Sprintf("test %v", i), func(t *testing.T) {
+			res := combinationSum2(ts.candidates, ts.target)
+
+			if !reflect.DeepEqual(ts.output, res) && len(ts.output) != len(res) {
+				t.Errorf("Expected: %v; got: %v", ts.output, res)
+			}
+		})
 	}
 }
